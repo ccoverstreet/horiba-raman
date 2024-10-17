6e5c3b9dfb73cc74c6e9c412e864ac15062d466e
ccoverstreet <coverst2@vols.utk.edu>
2024-10-17 19:19:10 -0400

    Updated figure and added helper function

--- src/example/example.py
+++ src/example/example.py
@@ -3,54 +3,49 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    #extra_spectra_to_files("Gd2O3_AlN_map_data.txt")
 
-    x, y, intensity = mapping.parse_image("Gd2O3_AlN_map.txt")
-    shift, pos, counts = mapping.parse_data_txt("Gd2O3_AlN_map_data.txt")
+img, extent_camera = mapping.parse_image_comb("Gd2O3_AlN_map.bmp", "Gd2O3_AlN_map.txt")
+shift, pos, counts = mapping.parse_data_txt("Gd2O3_AlN_map_data.txt")
 
+dim = mapping.determine_rectangular_map_dim(pos)
+extent = mapping.extract_extent_from_pos_rect(pos, dim)
 
-    maxes = []
-    for i, c in enumerate(counts):
-        m = mapping.extract_max_from_range(shift, c, 300, 400)
-        print(i, pos[i], m)
-        maxes.append(m)
+# Create an array and reshape based on rectangular mesh
+# of peak maxes corresponding to Gd2O3
+maxes = []
+for c in counts:
+    m = mapping.extract_max_from_range(shift, c, 300, 500)
+    maxes.append(m)
 
-    maxes = np.array(maxes)
-    max_map = np.reshape(maxes, (6, 12))
-    print(max_map)
+maxes = np.reshape(np.array(maxes), dim)
+print(maxes)
 
+# Throw everything into a plot to taste
+plt.figure(figsize=(10, 4))
+plt.subplot(121)
+plt.title(r"Overlay of Gd$_2$O$_3$ Raman peak", fontsize=16)
+plt.imshow(img, extent=extent_camera)
+plt.imshow(maxes, extent=extent, alpha=0.4, interpolation="bilinear")
+plt.annotate("AlN", (-280, 0), fontsize=16, ha="center")
+plt.annotate(r"Gd$_2$O$_3$", (295, 0), fontsize=16, ha="center")
+print(extent_camera)
+plt.xlim(extent_camera[0], extent_camera[1])
+plt.ylim(extent_camera[2], extent_camera[3])
+plt.xlabel(r"X [$\mu$m]", fontsize=16)
+plt.ylabel(r"Y [$\mu$m]", fontsize=16)
 
-    img = Image.open("Gd2O3_AlN_map.bmp")
-    #plt.imshow(np.flip(intensity), extent=(x[0], x[-1], y[-1], y[0]))
-    dx = np.abs(pos[0][0] - pos[1][0])
-    dy = np.abs(pos[0][1] - pos[13][1])
-    print(pos[0], pos[13])
+plt.subplot(122)
+plt.imshow(img, extent=extent_camera, cmap=plt.get_cmap("gist_grey"))
+plt.imshow(maxes, extent=extent, alpha=0.4, cmap=plt.get_cmap("gist_heat"))
+plt.xlim(extent_camera[0], extent_camera[1])
+plt.ylim(extent_camera[2], extent_camera[3])
 
-    plt.figure(figsize=(10, 4))
-    plt.subplot(121)
-    plt.title(r"Overlay of Gd$_2$O$_3$ Raman peak", fontsize=16)
-    plt.imshow(img, extent=(x[0], x[-1], y[-1], y[0]))
-    plt.imshow(max_map, extent=(pos[0][0]-dx/2, pos[-1][0] + dx/2, pos[-1][1] + dy/2, pos[0][1] - dy/2), alpha=0.4, interpolation="bilinear")
-    plt.annotate("AlN", (-280, 0), fontsize=16, ha="center")
-    plt.annotate(r"Gd$_2$O$_3$", (295, 0), fontsize=16, ha="center")
-    plt.xlim(x[0], x[-1])
-    plt.ylim(y[-1], y[0])
-    plt.xlabel(r"X [$\mu$m]", fontsize=16)
-    plt.ylabel(r"Y [$\mu$m]", fontsize=16)
+plt.xlabel(r"X [$\mu$m]", fontsize=16)
+plt.ylabel(r"Y [$\mu$m]", fontsize=16)
 
-    plt.subplot(122)
-    plt.imshow(img, extent=(x[0], x[-1], y[-1], y[0]))
-    plt.imshow(intensity, extent=(x[0], x[-1], y[-1], y[0]))
-    #plt.imshow(max_map, extent=(pos[0][0]-dx/2, pos[-1][0] + dx/2, pos[-1][1] + dy/2, pos[0][1] - dy/2), alpha=0.4, interpolation="bilinear")
-    plt.xlim(x[0], x[-1])
-    plt.ylim(y[-1], y[0])
+plt.tight_layout()
 
-    plt.xlabel(r"X [$\mu$m]", fontsize=16)
-    plt.ylabel(r"Y [$\mu$m]", fontsize=16)
+plt.savefig("Raman_mapping_postprocessing_demo.png")
+plt.show()
 
-    plt.tight_layout()
-
-    plt.savefig("Raman_mapping_postprocessing_demo.png")
-    plt.show()
 
